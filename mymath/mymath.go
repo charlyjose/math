672ac37e7adc6f1d2e147da4f7a8c6520ed2139b
charlyjose <23473681+charlyjose@users.noreply.github.com>
2019-10-05 12:20:28 +0530

    Added new file

---
+++ mymath/mymath.go
@@ -0,0 +1,52 @@
+package mymath
+
+// GCD returns the highest Common Factor of a number
+func GCD(a, b []int) int {
+	large := -1
+	l1 := len(a)
+	l2 := len(b)
+
+	switch l1 > l2 {
+	case false:
+		{ // l1<l2
+			for i := l2 - 1; i > -1; i-- {
+				for j := l1 - 1; j > -1; j-- {
+					if b[i] == a[j] {
+						if large < a[i] {
+							large = a[i]
+						}
+					}
+				}
+			}
+		}
+	case true:
+		{ // l1>l2
+			for i := l1 - 1; i > -1; i-- {
+				for j := l2 - 1; j > -1; j-- {
+					if a[i] == b[j] {
+						if large < a[i] {
+							large = a[i]
+						}
+					}
+				}
+			}
+		}
+	}
+	return large
+}
+
+// Factors return array of factors of a number
+func Factors(n int) []int {
+	half := n / 2
+	j := 0
+	a := [...]int{}
+	d := a[:]
+	for i := 1; i <= half; i++ {
+		if n%i == 0 {
+			d = append(d, i)
+			j++
+		}
+	}
+	d = append(d, n)
+	return d
+}
